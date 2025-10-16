ef5878cd9535c5d318c12430fdf089d66da0e196
Tilde <tildelundqvist01@gmail.com>
2025-10-16 16:25:57 +0200

    Finished report generator (#13)
    Finished report generator with pytests.

--- tests/python/test_generator_empty.py
+++ tests/python/test_generator_empty.py
@@ -44,21 +44,21 @@ def test_generated_file():
         <body>
             <h1>Statistics Report</h1>
 
-            <p><b>Mean:</b> <span class="">85.0</span></p>
-            <p><b>Median:</b> <span class="">86.5</span></p>
+            <p><b>Mean:</b> <span class="missing">Value/s can not be found</span></p>
+            <p><b>Median:</b> <span class="missing">Value/s can not be found</span></p>
 
             <p><b>Mode(s):</b></p>
             <ul>
-                <li>95</li><li>88</li><li>85</li><li>72</li>
+                <li class='missing'>Value/s can not be found</li>
             </ul>
 
-            <p><b>Variance:</b> <span class="">69.5</span></p>
-            <p><b>Standard Deviation:</b> <span class="">8.336666000266533</span></p>
+            <p><b>Variance:</b> <span class="missing">Value/s can not be found</span></p>
+            <p><b>Standard Deviation:</b> <span class="missing">Value/s can not be found</span></p>
 
             <p><b>Frequencies:</b></p>
             <table>
                 <tr><th>Value</th><th>Frequency</th></tr>
-                <tr><td>72</td><td>1</td></tr><tr><td>85</td><td>1</td></tr><tr><td>88</td><td>1</td></tr><tr><td>95</td><td>1</td></tr>
+                <tr><td colspan='2' class='missing'>Value/s can not be found</td></tr>
             </table>
         </body>
         </html>
